ba33d6e21c9f4b909b5c6e0c2b30dbcd4589750e
Maya Kreidieh <mayakreidieh@gmail.com>
2013-11-14 03:12:53 -0500

    Add digital output base element

--- server/server.js
+++ server/server.js
@@ -2,9 +2,29 @@ var socket = require('socket.io');
 var io = socket.listen(8080);
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
+  
   socket.on('input', function (data) {
-  	console.log('received other event data');
-    console.log(data);
+  	// console.log('received other event data');
+   //  console.log(data);
     socket.emit('digitalWrite', {msg:data});
   });
+
+  // writeToPin writes to a pin, given type
+  // type : digitalWrite   ->   digitalWrite(pin, value); 
+  socket.on('writeToPin', function(data){
+  	console.log('test write to arduino');
+  	console.log(data);
+  });
+
+  // InitPin rerouts message to arduino to call 
+  // pinMode(pin, value) 
+  // on corresponding arduino with given ID     
+  socket.on('initPin', function(data){
+    console.log('initalizing pin');
+    console.log(data);
+    socket.emit('initPin', {msg:data});
+  })
+
+
 });
+
