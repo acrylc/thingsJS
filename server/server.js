793a7c4d034471e9d72c615559badb80c34ca0bf
Maya Kreidieh <mayakreidieh@gmail.com>
2013-11-19 16:36:24 -0500

    Update server as mqtt client

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
+
+
+// Create a websocket server and listen
+// to port 8080
 var socket = require('socket.io');
 var io = socket.listen(8080);
+
+
+// Set the server to be an mqtt client that publishs
+// to the MQTT client messages that need to be routed
+// to the arduino
+var mqtt = require('mqtt');
+var client = mqtt.createClient(1883, '162.243.38.166');
+client.subscribe('arduino');
+
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   
@@ -9,11 +22,20 @@ io.sockets.on('connection', function (socket) {
     socket.emit('digitalWrite', {msg:data});
   });
 
-  // writeToPin writes to a pin, given type
+  // readFromPin reads a sensor's value and re-routes it to virtual element
+  // type : digitalWrite   ->   digitalWrite(pin, value); 
+  socket.on('readFromPin', function(data){
+    console.log('test write to arduino');
+    console.log(data);
+  });
+
+  // writeToPin reads an actuator's value and rerouts it to physical element
   // type : digitalWrite   ->   digitalWrite(pin, value); 
   socket.on('writeToPin', function(data){
   	console.log('test write to arduino');
   	console.log(data);
+    client.publish('b', JSON.stringify(data) );
+
   });
 
   // InitPin rerouts message to arduino to call 
@@ -21,10 +43,11 @@ io.sockets.on('connection', function (socket) {
   // on corresponding arduino with given ID     
   socket.on('initPin', function(data){
     console.log('initalizing pin');
-    console.log(data);
-    socket.emit('initPin', {msg:data});
-  })
+    console.log(JSON.stringify(data));
+    // socket.emit('initPin', {msg:data});
+    client.publish('b', JSON.stringify(data) );
 
+  })
 
 });
 
