6d8355d3a268f1a22d543b33532809325c05f0fe
Maya Kreidieh <mayakreidieh@gmail.com>
2013-11-20 01:37:44 -0500

    Create push button as higher level implementation of digital input, with example file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,15 @@
 
-console.log('starting');
-console.log('#di');
-$("#di")[0].valueChanged = function(){
-	console.log(this.value);
-	if (this.value>0){
-		$('#led')[0].turnOn();
-	};
-	if (this.value==0){
-		$('#led')[0].turnOff();
-	};
 
+// Testing the button and LED implementations 
+// Simple circuit, turn ON and OFF LED with button click
+var button = $('#di')[0];
+var led = $('#led')[0];
+
+button.onClick = function(){
+	if (led.isOn() ){
+		led.turnOff();
+	}
+	else {
+		led.turnOn();
+	};
 }
