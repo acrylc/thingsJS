08a3024ef668229781028b802e6a61a5a7450119
Maya Kreidieh <mayakreidieh@gmail.com>
2013-11-28 17:36:01 -0500

    Add flex and wiggly interactors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,14 @@
 
 // Testing the button and LED implementations 
 // Simple circuit, turn ON and OFF LED with button click
-var button = $('#di')[0];
-var led = $('#led')[0];
+// var button = $('#di')[0];
+// var led = $('#led')[0];
 
-button.onClick = function(){
-	if (led.isOn() ){
-		led.turnOff();
-	}
-	else {
-		led.turnOn();
-	};
-}
+// button.onClick = function(){
+// 	if (led.isOn() ){
+// 		led.turnOff();
+// 	}
+// 	else {
+// 		led.turnOn();
+// 	};
+// }
